4164be59c4f331a94795964a394b05a2ca9c8f7c
Abaseen Khan <arbaseen@gmail.com>
2025-04-10 13:12:58 -0400

    forgot to change metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,21 @@
 import type { Metadata } from 'next'
 import './globals.css'
 
+
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+    title: 'Bento',
+    description: 'A new way to anime',
+    generator: 'v0.dev',
 }
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode
+    children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    )
 }
