42257a8e49622ff2ef5d53d70a2382dd6a586f67
Abaseen Khan <arbaseen@gmail.com>
2025-04-11 22:24:31 -0400

    added missing style to tag-button

--- components/tag-button.tsx
+++ components/tag-button.tsx
@@ -11,7 +11,7 @@ export default function TagButton({ label, isSelected, onClick }: TagButtonProps
         <button
             onClick={onClick}
             className={`px-4 py-1 rounded-lg font-mono border border-[#4a4023]/50 transition-all ${isSelected
-                    ? "bg-[#4a4023] text-white shadow-sm"
+                    ? "bg-[#4a4023] text-white shadow-sm hover:bg-[#3b341c]"
                     : "text-[#4a4023] hover:bg-[#e6e3df] hover:border-[#4a4023] shadow-sm"
                 }`}
         >
