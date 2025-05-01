7f93896beac0eaaeab5608bdaa1860a3a017d8ea
Abaseen Khan <arbaseen@gmail.com>
2025-05-01 15:04:42 -0400

    added speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout({
         <html lang="en">
             <body>{children}</body>
             <Analytics />
+            <SpeedInsights />
         </html>
     )
 }
