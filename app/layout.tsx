2530ddf5ac9072d7f2e28e027d6feca777080716
Abaseen Khan <arbaseen@gmail.com>
2025-05-01 09:33:00 -0400

    changed tab icon trying to add preview image

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,23 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-    title: 'Bento',
+    title: 'Bento Anime',
     description: 'A new way to anime',
     generator: 'v0.dev',
+    openGraph: {
+        title: "Bento Anime",
+        description: "A new way to anime",
+        url: "https://bentoanime.com",
+        type: "website",
+        images: [
+            {
+                url: "/preview.png",
+                width: 1200,
+                height: 630,
+                alt: "Bento Site Preview",
+            },
+        ],
+    }
 }
 
 export default function RootLayout({
