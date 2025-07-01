6f605af2adb2d3c5ff60240ab98993f272220b1d
Abaseen Khan <arbaseen@gmail.com>
2025-07-01 10:40:06 -0400

    fixed navbar for mobile

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,11 +2,11 @@ export default function Navbar( {onJoinWaitlist} : any) {
     return (
         <div className="flex justify-center">
 
-        <div className="w-full px-10 m-4 items-center bg-white flex flex-row max-w-5xl justify-between">
+        <div className="w-full px-10 m-4 items-center bg-white flex flex-row max-w-5xl justify-center md:justify-between">
             <button onClick={onJoinWaitlist} className="text-sm p-2 rounded-md border border-mySecondary hover:border-mySecondary transition-colors">
                 join the waitlist
             </button>
-            <div className="flex flex-row gap-12">
+            <div className="flex-row gap-12 hidden md:flex md:justify-center">
                 <a href="/" className="font-semibold">
                     recommendations
                 </a>
