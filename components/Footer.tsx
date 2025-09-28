0c17caafe18cdaf3005cde27ae24512891e3005d
Abaseen Khan <arbaseen@gmail.com>
2025-09-28 16:19:50 -0400

    removed wording from footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,14 +4,14 @@ import Link from "next/link"
 export default function Footer() {
 
     return (
-        <main className="grid grid-cols-3 w-full items-center p-5">
+        <main className="w-full items-center p-5">
 
-            <div className="justify-self-start text-4xl font-medium">bento</div>
+            {/* <div className="justify-self-start text-4xl font-medium">bento</div> */}
 
-            <div className="justify-self-center flex flex-row gap-4">
+            <div className="justify-center flex flex-row gap-4">
 
                 <Link href="https://x.com/animebento">
-                    <Image src="/images/x.svg" width={24} height={24} alt="x logo" />
+                    <Image src="/images/x.svg" width={26} height={26} alt="x logo" />
                 </Link>
 
                 <Link href="https://www.instagram.com/bentoxanime">
@@ -20,7 +20,7 @@ export default function Footer() {
 
             </div>
 
-            <div className="justify-self-end text-xl font-medium">a new way to anime</div>
+            {/* <div className="justify-self-end text-xl font-medium">a new way to anime</div> */}
 
         </main>
     )
